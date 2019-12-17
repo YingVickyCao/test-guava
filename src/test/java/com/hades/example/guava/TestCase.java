11f8150a2ac9d4904b30b4d7df8295d13d8a5b6d
Vicky <ying.vicky.cao@outlook.com>
2019-12-17 15:51:28 +0800

    refactor

--- src/test/java/com/hades/example/guava/TestCase.java
+++ src/test/java/com/hades/example/guava/TestCase.java
@@ -8,11 +8,19 @@ public class TestCase {
 
     @Test
     public void test() {
+        test_checkState();
+    }
+
+    private void test_checkState() {
         int num1 = 1;
         int num2 = 2;
         checkState(num1 != num2, "num1 is equal num2");
 
         num2 = 1;
+
+        /**
+         * checkState: When condition is false, print error msg
+         */
 //        checkState(num1 != num2, "num 1 is not equal num2");// ERROR:java.lang.IllegalStateException: num 1 is equal num2
         checkState(num1 != num2, "num1 %s is equal num2 %s", num1, num2);// ERROR:java.lang.IllegalStateException: num1 1 is equal num2 1
     }
